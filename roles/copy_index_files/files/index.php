6efe5f4819218aff018441168585cc7e8a7dcd99
Emmanuel Keziah <keziahema@gmail.com>
2024-01-11 11:50:05 +0100

    Initial commit

---
+++ roles/copy_index_files/files/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>A SIMPLE PHP FILE </title>
+<style>
+    section {
+        color: blue;
+        margin: 20px;
+    }
+    p {
+        font-weight: bold;
+        font-style: italic;
+    }
+</style>
+</head>
+<body>
+<h1>THIS IS A SIMPLE PHP FILE</h1>
+<section>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <p>Nulla facilisi. Sed euismod, nunc id ultrices eleifend, nunc nunc tincidunt mauris, id lacinia nisl nunc id nunc.</p>
+</section>
+</body>
+</html>
